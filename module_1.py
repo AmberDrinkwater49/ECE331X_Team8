a9c541a4b539e6f9049f78d82bad1d5e5da0555e
Verit Li <verit.li@gmail.com>
2025-10-29 19:55:08 -0400

    module 1 python file init

---
+++ module_1.py
@@ -0,0 +1,18 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from scipy import signal
+import time
+import adi
+
+# ----------------------------------------------------------
+# Code for generating a spectrogram based on the intermittent 
+# wireless transmission of AcuRite Wireless Digital Weather Thermometer
+# and received by a Pluto SDR
+#
+# Team 8
+# Amber Drinkwater, Kaz Coble, Verit Li
+# 10-29-2025
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
