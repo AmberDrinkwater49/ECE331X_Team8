5832324638e30f941a09df2216fc1a41728fa7fb
Amber <amberdrinkwater112@gmail.com>
2025-11-02 18:20:19 -0500

    Integrated Prof spectrogram and data capture. Code is ready to test

--- module_1.py
+++ module_1.py
@@ -46,3 +46,54 @@ sdr.gain_control_mode_chan0 = "slow_attack" #Mode of receive path AGC(Automatic 
 
 # Configuration of data channel
 sdr.rx_enabled_channels = [0] #enable only one rx channel
+
+
+def dataCapture() -> list:
+    #This function begins the capture of data at given frequencies to decode the thermometers
+    data = sdr.rx()
+    Rx_0 = data[0]
+    return Rx_0
+    
+
+
+# ----------------------------------------------------------
+# Define the handmade spectrogram function
+def myspectrogram(data,N,M,Fs):
+    
+    # Calculate number of windows to be processed
+    num_windows = int((len(data) - (M/2)) // (N - (M/2)))
+    
+    # Generate Hamming window
+    hamming_window = np.hamming(N)
+
+    # Define time instances for FFT slices
+    t_spectro = np.arange(0, (num_windows)*(N*(1/Fs)), N*(1/Fs))
+
+    # Define FFT bin frequencies (normalized by 2\pi)
+    f_spectro = np.arange(0, 1, 1/N)
+
+    # Divide up data into blocks of size N, with M/2 overlap with previous
+    # block and M/2 overlap with next block, and multiple with hamming window
+    spectrogram_results = np.zeros((num_windows,N))
+    for i in range(num_windows):
+        start_ind = i*(int(N - (M/2)))
+        seg = data[start_ind:(start_ind + N)]
+        windowed_seg = seg*hamming_window
+        abs_fft_result = np.abs(np.fft.fft(windowed_seg))
+        spectrogram_results[i] = abs_fft_result
+
+    return t_spectro, f_spectro, spectrogram_results
+
+
+
+
+def main():
+    data = dataCapture()
+    t_spectro, f_spectro, specresults =  myspectrogram(data, 256, 64, Fs)
+
+    # ----------------------------------------------------------
+    # Plot handmade version of spectrogram using color mesh plotting routine
+    plt.pcolormesh(t_spectro,f_spectro,np.log10(specresults.T),shading='auto')
+    plt.ylabel('Frequency [Hz]')
+    plt.xlabel('Time [seconds]')
+    plt.show()
