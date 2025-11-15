d8bf5613cd5e51fb49e7873456e452e2aedace7c
Amber Drinkwater <amberdrinkwater112@gmail.com>
2025-11-15 17:47:17 -0500

    Found the offset, trying to fix it now

--- module_3.py
+++ module_3.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
-from scipy import signal
+from scipy import signal, ndimage
 import time
+import math
 #import adi
 import json
 
@@ -27,7 +28,7 @@ Fs = (int)(521e3) #sampling frequency of ADC in samples per second original: 521
 buffer_size = (int)(2 ** 12) # previously 2 ** 12
 NFFT = 1024 #original: 1024
 noverlap = NFFT // 8 #original: 64
-
+FFT_LIMIT = 2500
 
 
 #raw_data = [0] * ((int)(Fs * num_seconds))
@@ -80,6 +81,80 @@ def plot_constellation(data):
     plt.grid(True)
     plt.show()
 
+def plot_fft(data):
+    fft = np.fft.fft(data)
+    # Get the corresponding frequencies
+    frequencies = np.fft.fftfreq(len(data), d=1/Fs)
+
+    # Plot the magnitude spectrum (absolute value of the FFT output)
+    plt.figure(figsize=(10, 5))
+    plt.plot(frequencies, np.abs(fft))
+    plt.title('Magnitude Spectrum of the Signal')
+    plt.xlabel('Frequency (Hz)')
+    plt.ylabel('Magnitude')
+    plt.grid(True)
+    plt.xlim(0, FFT_LIMIT) # Show only positive frequencies
+    plt.show()
+    return [fft, frequencies]
+
+# def offsetFinder(data):
+# # Estimation of error
+#     fftOrder = 2**10
+#     k = 1
+#     modulationOrder = 2
+#     frequencyRange = np.linspace(-Fs/2,Fs/2,fftOrder)
+#     # Precalculate constants
+#     offsetEstimates = np.zeros(int(np.floor(len(data)/fftOrder)),1)
+#     indexToHz = Fs/(modulationOrder*fftOrder)
+#     for est in range(1,len(offsetEstimates)):
+#         # Increment indexes
+#         timeIndex = np.linspace(k,k+fftOrder-1)
+#         k = k + fftOrder
+#         # Remove modulation effects
+#         sigNoMod = data ** modulationOrder
+#         # Take FFT and ABS
+#         freqHist = np.abs(np.fft.fft(sigNoMod))
+#         # Determine most likely offset
+#         maxInd = np.argmax(freqHist)
+#         offsetInd = maxInd - 1
+#         if maxInd>=fftOrder/2: # Compensate for spectrum shift
+#             offsetInd = offsetInd - fftOrder
+        
+#         # Convert to Hz from normalized frequency index
+#     offsetEstimates[est] = offsetInd * indexToHz
+#     return(offsetEstimates(0))
+
+
+def find_centroid(fft, frequencies):
+    #Integrate over the fft to find the spike where our signal is and determine at which frequency that is!
+
+# Version Textbook
+    # fftOrder = 2 ** 10
+    # frequencyRange = np.linspace(-Fs/2,Fs/2,fftOrder)
+    # offsetEstimates = np.zeros(np.floor(len(fft)/fftOrder),1)
+    # offsetIndex = np.argmax(np.abs(fft**2)) - 1
+    # indexToHz = Fs/(2*fftOrder)
+
+    # if offsetIndex>=fftOrder/2:
+    #     offsetIndex = offsetIndex - fftOrder
+
+    # print(indexToHz*offsetIndex)
+    # return offsetIndex*indexToHz
+
+# Version Amber
+    sum = 0
+    sum2 = 0
+    final_freq = 0
+    for x in range(0,FFT_LIMIT):
+        sum += np.abs(fft[x])
+    for x in range(0, FFT_LIMIT):
+        sum2 += np.abs(fft[x])
+        if sum2 >= sum/2:
+            print(frequencies[x])
+            return x
+
+
+
 def main():
     total_time = 67
 
@@ -130,13 +205,18 @@ def main():
     #generate low pass filter
     #coeffs = signal.firwin(numtaps,[Fcutoff_low/Fnyquist,Fcutoff_high/Fnyquist],fs=2*Fs,pass_zero=False)
     #convolve the filter and data to apply filter
-    #signal_data = signal.fftconvolve(coeffs, signal_data)
+    [fft, frequencies] = plot_fft(signal_data)
+    offset = find_centroid(fft,frequencies)
+    impulse = np.zeros(len(signal_data))
+    impulse[offset] = 1
+    signal_data = np.convolve(signal_data, impulse)
+
     
     plot_magnitude(signal_data, signal_time)
     plot_phase(signal_data, signal_time)
-    #plt.specgram(signal_data, Fs=Fs, NFFT=NFFT, noverlap=noverlap, Fc=Fc)
-    #plt.savefig('plot.png')
-    #plt.show()
+    plt.specgram(signal_data, Fs=Fs, NFFT=NFFT, noverlap=noverlap, Fc=Fc)
+    plt.savefig('plot.png')
+    plt.show()
     plot_constellation(signal_data)
 
 
